11a686340da4aee2d2ec3f136f2b6c5e85122602
mark <mark.fischer0404@gmail.com>
2022-05-26 00:08:32 +0200

    sprite sachen am machen (nicht das getränk)

--- src/main/java/com/prog2/uwupatch1/Start.java
+++ src/main/java/com/prog2/uwupatch1/Start.java
@@ -5,21 +5,44 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Start extends Application {
 
+    private Pane pane = new Pane();
+    private Sprite playerInDungeon = new Sprite(300, 750, 40, 40, "playerInDungeon", Color.GREEN);
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Start.class.getResource("StartStage.fxml"));
         Scene scene = new Scene(root);
+        pane.getChildren().add(playerInDungeon);
         stage.setResizable(false);
         stage.setFullScreen(true);
         stage.setFullScreenExitHint("");
         stage.setTitle("DungeonTop UWU");
         stage.setScene(scene);
         stage.show();
+
+        scene.setOnKeyPressed( e -> {
+            switch(e.getCode()){
+                case W:
+                    playerInDungeon.moveUp();
+                    break;
+                case A:
+                    playerInDungeon.moveLeft();
+                    break;
+                case S:
+                    playerInDungeon.moveDown();
+                    break;
+                case D:
+                    playerInDungeon.moveRight();
+                    break;
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -28,6 +51,33 @@ public class Start extends Application {
         }
     }
 
+    /**
+     * Klasse Sprite erzeugt ein grafisches Viereck, beinhaltet ebenso die Methoden für die Bewegung von Objekten.
+     */
+    private static class Sprite extends Rectangle{
+        final String type;
+
+        Sprite(int x, int y, int w, int h, String type, Color color){
+            super(w, h, color);
+            this.type = type;
+            setTranslateX(x);
+            setTranslateY(y);
+        }
+
+        //movement
+        void moveLeft(){
+            setTranslateX(getTranslateX() - 5);
+        }
+        void moveRight(){
+            setTranslateX(getTranslateX() + 5);
+        }
+        void moveUp(){
+            setTranslateY(getTranslateY() + 5);
+        }
+        void moveDown(){
+            setTranslateY(getTranslateY() - 5);
+        }
+    }
 
 
 
