49ce37f50b6d49782fea0a98d8cc0d9e8cec6cf1
Niclas <rieckersniclas@gmail.com>
2022-06-08 15:09:28 +0200

    Scheisse diese Erstellung

---
+++ src/main/java/de/prog2/uwupatch1/contoller/LoadGamesStageController.java
@@ -0,0 +1,22 @@
+package de.prog2.uwupatch1.contoller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoadGamesStageController implements Initializable {
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  {@code null} if the location is not known.
+     * @param resources The resources used to localize the root object, or {@code null} if
+     *                  the root object was not localized.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
